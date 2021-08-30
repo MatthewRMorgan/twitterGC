2061b308d203229625c998c2a0ccff8153306f0b
MatthewRMorgan <matthewmorgan.r@gmail.com>
2021-08-29 22:01:42 -0700

    added search to automation

--- tests/scrapeTest.py
+++ tests/scrapeTest.py
@@ -15,6 +15,7 @@ from selenium.webdriver import Chrome
 driver = Chrome(executable_path="C:/Users/matth/Downloads/chromedriver_92/chromedriver.exe")
 driver.get("https://www.twitter.com/login")
 
+#sleep to allow webpage to load
 sleep(2)
 
 #login to twitter using xpath, selenium, python
@@ -25,3 +26,17 @@ password = driver.find_element_by_xpath('//input[@name="session[password]"]')
 password.send_keys(config.my_pw)
 
 password.send_keys(Keys.RETURN)
+
+sleep(2)
+
+#search for desired keys
+search_keyword = driver.find_element_by_xpath('//input[@aria-label="Search query"]')
+search_keyword.send_keys('#GCChat')
+search_keyword.send_keys(Keys.RETURN)
+
+##navigate to tabs to sort twitter data
+# driver.find_element_by_link_text('Latest').click()
+
+#get tweets on current page
+tweets = driver.find_elements_by_xpath('//div[@data-testid="tweet"]')
+
