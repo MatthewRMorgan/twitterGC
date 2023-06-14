4d81a046a7f5a6bcd512af26b33ccac825fa5812
MatthewRMorgan <60022401+MatthewRMorgan@users.noreply.github.com>
2023-06-14 14:26:12 -0700

    changed quait 8 to 9

--- docs/transiat.js
+++ docs/transiat.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat8.js'], function (APIConstructor, iatExtension) {
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function (APIConstructor, iatExtension) {
 	var API = new APIConstructor();
 	var global = API.getGlobal();
 
