4dfa4b78a7626514bad7c1007f19c5659cf84a4d
MatthewRMorgan <60022401+MatthewRMorgan@users.noreply.github.com>
2023-06-14 19:34:35 -0700

    small edits

--- docs/transiat.js
+++ docs/transiat.js
@@ -35,27 +35,6 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@master/IAT/qu
 			//Stimulus css
 			stimulusCss: { color: '#31b404', 'font-size': '2.3em' }
 		},
-		attribute2:
-		{
-			name: 'Good words', //Will appear in the data.
-			title: {
-				media: { word: 'Good words' }, //Name of the category presented in the task.
-				css: { color: '#0000FF', 'font-size': '1.8em' }, //Style of the category title.
-				height: 4 //Used to position the "Or" in the combined block.
-			},
-			stimulusMedia: [ //Stimuli content as PIP's media objects
-				{ word: 'Friend' },
-				{ word: 'Smiling' },
-				{ word: 'Adore' },
-				{ word: 'Joyful' },
-				{ word: 'Pleasure' },
-				{ word: 'Friendship' },
-				{ word: 'Happy' },
-				{ word: 'Attractive' }
-			],
-			//Stimulus css (style)
-			stimulusCss: { color: '#0000FF', 'font-size': '2.3em' }
-		},
 		attribute1:
 		{
 			name: 'Bad words', //Will appear in the data.
@@ -77,18 +56,40 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@master/IAT/qu
 			//Stimulus css
 			stimulusCss: { color: '#0000FF', 'font-size': '2.3em' }
 		},
+		attribute2:
+		{
+			name: 'Good words', //Will appear in the data.
+			title: {
+				media: { word: 'Good words' }, //Name of the category presented in the task.
+				css: { color: '#0000FF', 'font-size': '1.8em' }, //Style of the category title.
+				height: 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia: [ //Stimuli content as PIP's media objects
+				{ word: 'Friend' },
+				{ word: 'Smiling' },
+				{ word: 'Adore' },
+				{ word: 'Joyful' },
+				{ word: 'Pleasure' },
+				{ word: 'Friendship' },
+				{ word: 'Happy' },
+				{ word: 'Attractive' }
+			],
+			//Stimulus css (style)
+			stimulusCss: { color: '#0000FF', 'font-size': '2.3em' }
+		},
+		
 
 		base_url: {//Where are your images at?
-			image: '/implicit/user/demo.us/demo.trans.0001/images'
+			image: 'https://github.com/MatthewRMorgan/twitterGC/tree/main/docs/resources/pictures/IAT/'
 		},
 		//isTouch: global.isTouch,
 		//attribute1, and attribute2 will be replaced with the name of attribute1 and attribute2.
 		//categoryA is the name of the category that is found to be associated with attribute1,
 		//and categoryB is the name of the category that is found to be associated with attribute2.
-		fb_strong_Att1WithCatA_Att2WithCatB: 'Your data suggest strong automatic preference for categoryB over categoryA.',
-		fb_moderate_Att1WithCatA_Att2WithCatB: 'Your data suggest moderate automatic preference for categoryB over categoryA.',
-		fb_slight_Att1WithCatA_Att2WithCatB: 'Your data suggest weak automatic preference for categoryB over categoryA.',
-		fb_equal_CatAvsCatB: 'Your data suggest no automatic preference between categoryA and categoryB.'
+		//fb_strong_Att1WithCatA_Att2WithCatB: 'Your data suggest strong automatic preference for categoryB over categoryA.',
+		//fb_moderate_Att1WithCatA_Att2WithCatB: 'Your data suggest moderate automatic preference for categoryB over categoryA.',
+		//fb_slight_Att1WithCatA_Att2WithCatB: 'Your data suggest weak automatic preference for categoryB over categoryA.',
+		//fb_equal_CatAvsCatB: 'Your data suggest no automatic preference between categoryA and categoryB.'
 	});
 });
 
